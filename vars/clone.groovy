2f1725699b7c5a01066421ec8b857ad14caaf8fb
thanewalaess-lab <thanewalaess@gmail.com>
2026-04-06 10:35:55 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,3 +1,5 @@
+def call(String url, String branch) {
  echo "this is cloning the code"
                 git url: "https://github.com/thanewalaess-lab/django-notes-app.git" ,branch:"main"
                 echo "code clone sucessful"
+}
